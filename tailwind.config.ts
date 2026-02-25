d1d4b978ea1cab2e1674a55441add85f2afe9889
Atul Ballav Kharel <atulballavk@gmail.com>
2026-02-25 16:38:41 +0545

    changes to pack and globsl

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,29 +11,10 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        primary: {
-          DEFAULT: '#144a7b',
-          light: '#1E88E5',
-          dark: '#0d3454',
-        },
-        secondary: {
-          DEFAULT: '#00bf63',
-          dark: '#009950',
-        },
-        'surface': '#fafafa',
-        'surface-alt': '#f2f5f6',
-      },
-      spacing: {
-        'navbar-mobile': '121px',
-        'navbar-desktop': '182px',
-      },
-      screens: {
-        'navbar-bp': '901px', // Custom breakpoint for navbar
       },
     },
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [],
 };
+
 export default config;
--- types/pdfjs.d.ts
+++ types/pdfjs.d.ts
@@ -2,3 +2,8 @@ declare module 'pdfjs-dist/legacy/build/pdf.worker.entry' {
   const worker: any;
   export default worker;
 }
+
+declare module 'pdfjs-dist/build/pdf' {
+  const pdfjsLib: any;
+  export = pdfjsLib;
+}
